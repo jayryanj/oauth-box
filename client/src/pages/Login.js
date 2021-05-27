82eb20b91523f47e696a521d8c1510e245ac8d60
jayryanj <43054912+jayryanj@users.noreply.github.com>
2021-05-26 15:19:55 -1000

    Added transition for login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import "../styles/Login.css";
 import { 
     Button,
+    Fade,
     Link,
     TextField,
     Typography
@@ -14,31 +15,33 @@ import ArrowBackOutlinedIcon from '@material-ui/icons/ArrowBackOutlined';
 class Login extends React.Component {
     render() {
         return(
-            <Container className="login-container-main">
-                <Container className="login-container-home">
-                    <Button className="login-button" component={NavLink} to="/" >
-                        <ArrowBackOutlinedIcon  /> Back to Home
-                    </Button>
-                </Container>
+            <Fade in={true} >
+                <Container className="login-container-main">
+                    <Container className="login-container-home">
+                        <Button className="login-button" component={NavLink} to="/" >
+                            <ArrowBackOutlinedIcon  /> Back to Home
+                        </Button>
+                    </Container>
 
-                <Typography className="login-header" variant="h3">
-                    <b>Log in</b> to OAuth Box
-                </Typography>
-                <p>
-                    Don't have an account? <Link>Register</Link>
-                </p>
-                <form noValidate autoComplete="off">
-                    <div>
-                        <TextField variant="outlined" className="login-form" fullWidth id="standard-basic" label="Email" />
-                    </div>
-                    <div>
-                        <TextField type="password" variant="outlined" className="login-form" fullWidth id="standard-basic" label="Password" />
-                    </div>
-                    <div>
-                        <Button fullWidth size="large" className="login-form login-submit" >Login</Button>
-                    </div>
-                </form>
-            </Container>
+                    <Typography className="login-header" variant="h3">
+                        <b>Log in</b> to OAuth Box
+                    </Typography>
+                    <p>
+                        Don't have an account? <Link>Register</Link>
+                    </p>
+                    <form noValidate autoComplete="off">
+                        <div>
+                            <TextField variant="outlined" className="login-form" fullWidth id="standard-basic" label="Email" />
+                        </div>
+                        <div>
+                            <TextField type="password" variant="outlined" className="login-form" fullWidth id="standard-basic" label="Password" />
+                        </div>
+                        <div>
+                            <Button fullWidth size="large" className="login-form login-submit" >Login</Button>
+                        </div>
+                    </form>
+                </Container>
+            </Fade>
         );
     }
 }
